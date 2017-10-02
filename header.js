fbdca3477c383d9f51252471b35e78fb9cf36954
Hkuuskla <hkuuskla@gmail.com>
2017-10-02 22:41:36 +0300

    style

--- header.js
+++ header.js
@@ -3,8 +3,8 @@
 2. Vaata mis positsioonis on scrollimine
 3. Kui scrollimine on 0 siis ta on topis ja siis eemaldame klassi scrolled.
 4. Kui scrollimine on suurem kui 0, lisan klassi scrolled.
-
 */
+
 var header = document.getElementById("header");
 var body = document.getElementsByTagName("body")[0];
 
@@ -16,3 +16,52 @@ body.onscroll = function(){
         header.classList.add("scrolled");
     }
 };
+
+
+$(".home-button").click(function() {
+    var scrollTop = $("#home").offset().top;
+    $('body').animate({
+        scrollTop: scrollTop
+    }, 2000);
+});
+
+$(".about-button").click(function() {
+    var scrollTop = $("#about").offset().top;
+    $('body').animate({
+        scrollTop: scrollTop
+    }, 2000);
+});
+
+$(".projects-button").click(function() {
+    var scrollTop = $("#projects").offset().top;
+    $('body').animate({
+        scrollTop: scrollTop
+    }, 1500);
+});
+
+$(".contacts-button").click(function() {
+    var scrollTop = $("#contacts").offset().top;
+    $('body').animate({
+        scrollTop: scrollTop
+    }, 2000);
+});
+
+/*
+
+var buttons = ["home", "about", "projects", "contacts"];
+
+
+for (var i = 0; i < buttons.length;  i++) {
+    var buttonSelector = "." + buttons[i] + "-button";
+    var pageSelector = "#" + buttons[i];
+
+    $(buttonSelector).click(function() {
+        var scrollTop = $(pageSelector).offset().top;
+        $('body').animate({
+            scrollTop: scrollTop
+        }, 2000);
+    });
+}
+*/
+
+
